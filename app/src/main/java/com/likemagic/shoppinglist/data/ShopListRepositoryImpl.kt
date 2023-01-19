bc3bd4574c9d5af89853a661c4a76805c08e3f74
LikeMagic85 <like-magic@yandex.ru>
2023-01-19 12:07:16 +0300

    Adapter create

--- app/src/main/java/com/likemagic/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/likemagic/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -4,18 +4,19 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.likemagic.shoppinglist.domain.ShopItem
 import com.likemagic.shoppinglist.domain.ShopListRepository
+import kotlin.random.Random
 
 object ShopListRepositoryImpl:ShopListRepository {
 
-    private val shopList = mutableListOf<ShopItem>()
+    private val shopList = sortedSetOf<ShopItem>({ o1, o2 -> o1.ID.compareTo(o2.ID)})
 
     private val shopListLD = MutableLiveData<List<ShopItem>>()
 
     private var autoIncrementId = 0
 
     init {
-        for (i in 0 until 10){
-            val item = ShopItem("item$i", i, true)
+        for (i in 0 until 1000){
+            val item = ShopItem("name$i", i, Random.nextBoolean())
             addShopItem(item)
         }
     }
